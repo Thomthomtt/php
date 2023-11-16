e2fc55b4849cf77ea181b4f5c7b61504badd0e16
Thomthomtt <109621131+Thomthomtt@users.noreply.github.com>
2023-11-15 22:27:52 -0300

    Delete aula directory

--- aula/conexao.php
+++
@@ -1,8 +0,0 @@
-<?php
-define ("host", "127.0.0.1");
-define ("usuario", "root");
-define ("senha","");
-define ("db", "php_login");
-
-$conexao = mysqli_connect(host, usuario, senha, db) or die (mysqli_erro());
-?>
